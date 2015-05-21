29b1fdc827001c47a4d72202f89ff91ec4f815fa
unknown <chaiphes@.itoneco.com>
2015-05-21 23:39:04 +0700

    build faq page

---
+++ application/views/faq.php
@@ -0,0 +1,76 @@
+<div class="row">
+	<div class="col-xs-12 col-sm-12 col-md-12">
+		<div class="panel panel-default">
+			<div class="panel-heading"><h2>F.A.Q.</h2></div>
+			<div class="panel-body">
+				
+				
+				<div class="list-group">
+					<div class="list-group-item">
+						<h4 class="list-group-item-heading">How can I invest with forexdblot.com ?</h4>
+						<p class="list-group-item-text">
+							To make a investment you must first become a member of forexdblot.com hyip. Once you are signed up, you can make your first deposit. All deposits must be made through the Members Area. You can login using the member username and password you receive when signup.
+						</p>
+					</div>
+					
+					<div class="list-group-item">
+						<h4 class="list-group-item-heading">I wish to invest with forexshare.biz but I don't have an any ecurrency account. What should I do?</h4>
+						<p class="list-group-item-text">
+							You can open a free PerfectMoney account here: <a href="https://www.perfectmoney.com">www.perfectmoney.com</a>
+						</p>
+					</div>
+					
+					<div class="list-group-item">
+						<h4 class="list-group-item-heading">How do I open my forexshare.biz HYIP Account?</h4>
+						<p class="list-group-item-text">
+							It's quite easy and convenient. Follow this link, fill in the registration form and then press "Register".
+						</p>
+					</div>
+					
+					<div class="list-group-item">
+						<h4 class="list-group-item-heading">Which e-currencies do you accept?</h4>
+						<p class="list-group-item-text">
+							We accept Paysbuy, PerfectMoney and OKPay.
+						</p>
+					</div>
+					
+					<div class="list-group-item">
+						<h4 class="list-group-item-heading">How can I withdraw funds?</h4>
+						<p class="list-group-item-text">
+							Login to your account using your username and password and check the Withdraw section.
+						</p>
+					</div>
+					
+					<div class="list-group-item">
+						<h4 class="list-group-item-heading">How long does it take for my deposit to be added to my account?</h4>
+						<p class="list-group-item-text">
+							Your account will be updated as fast, as you deposit.
+						</p>
+					</div>
+					
+					<div class="list-group-item">
+						<h4 class="list-group-item-heading">How long does it take for my withdraw to e-currency account?</h4>
+						<p class="list-group-item-text">
+							????
+						</p>
+					</div>
+					
+				</div>
+				
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+	$('div .list-group-item')
+	.css('cursor', 'pointer')
+	.css('padding-top', '40px')
+	.css('padding-bottom', '40px')
+	.hover(function(){
+		$(this).css('background-color', '#f5f5f5');
+	}, function(){
+		$(this).css('background-color', 'white');
+	})
+</script>
